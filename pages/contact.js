3979250ea70cca3653301a8b099ee3a58f4d9659
Yusuf Kınataş <yusufkinatas@gmail.com>
2020-06-12 17:55:11 +0300

    add links to contact

--- pages/contact.js
+++ pages/contact.js
@@ -6,16 +6,30 @@ function Contact() {
       <div>
         <div>Wanna get in touch?</div>
         <div>
-          Send an email to me at <a>yusufkinatas@gmail.com</a>
+          Send an email to me at{' '}
+          <a href="mailto:yusufkinatas@gmail.com" target="_blank" rel="noopener noreferrer">
+            yusufkinatas@gmail.com
+          </a>
         </div>
         <div>Don't worry. I check my emails daily.</div>
         <br />
         <div>
-          You can also connect with me on <a>Linkedin</a> in seconds
+          You can also connect with me on{' '}
+          <a
+            href="https://www.linkedin.com/in/yusuf-kinatas/"
+            target="_blank"
+            rel="noopener noreferrer">
+            Linkedin
+          </a>{' '}
+          in seconds
         </div>
         <br />
         <div>
-          Check my <a>Github</a> for the latest projects.
+          Check my{' '}
+          <a href="https://github.com/yusufkinatas" target="_blank" rel="noopener noreferrer">
+            Github
+          </a>{' '}
+          for the latest projects.
         </div>
       </div>
     </Page>
