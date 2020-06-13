9fe98ba9ea043a9e6529219ae475d23266afe2a4
Yusuf Kınataş <yusufkinatas@gmail.com>
2020-06-13 12:28:36 +0300

    Fix navbar background style

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -21,27 +21,29 @@ function MainLayout({ children, route }) {
         />
       </Head>
       {route !== '/work/[id]' && (
-        <div className={styles.navbar}>
-          <Link href="/">
-            <a>
-              <div className={clsx(route === '/' && styles.active)}>about</div>
-            </a>
-          </Link>
-          <Link href="/work">
-            <a>
-              <div className={clsx(route === '/work' && styles.active)}>work</div>
-            </a>
-          </Link>
-          <Link href="/resume">
-            <a>
-              <div className={clsx(route === '/resume' && styles.active)}>resume</div>
-            </a>
-          </Link>
-          <Link href="/contact">
-            <a>
-              <div className={clsx(route === '/contact' && styles.active)}>contact</div>
-            </a>
-          </Link>
+        <div className={styles.navbarWrapper}>
+          <div className={styles.navbar}>
+            <Link href="/">
+              <a>
+                <div className={clsx(route === '/' && styles.active)}>about</div>
+              </a>
+            </Link>
+            <Link href="/work">
+              <a>
+                <div className={clsx(route === '/work' && styles.active)}>work</div>
+              </a>
+            </Link>
+            <Link href="/resume">
+              <a>
+                <div className={clsx(route === '/resume' && styles.active)}>resume</div>
+              </a>
+            </Link>
+            <Link href="/contact">
+              <a>
+                <div className={clsx(route === '/contact' && styles.active)}>contact</div>
+              </a>
+            </Link>
+          </div>
         </div>
       )}
       <AnimatePresence>{children}</AnimatePresence>
