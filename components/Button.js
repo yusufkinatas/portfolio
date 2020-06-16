d41f1db56c8335266574b32d822af2dc31fae16d
Yusuf Kınataş <yusufkinatas@gmail.com>
2020-06-16 06:33:33 +0300

    Add a tag support to button

--- components/Button.js
+++ components/Button.js
@@ -2,7 +2,15 @@ import Link from 'next/link';
 import styles from '../styles/components/button.module.scss';
 import clsx from 'clsx';
 
-function Button({ link, className, children }) {
+function Button({ link, renderATag, className, children }) {
+  if (renderATag) {
+    return (
+      <a href={link} target="blank" rel="noopener noreferrer">
+        <div className={clsx(styles.root, className)}>{children}</div>
+      </a>
+    );
+  }
+
   return (
     <Link href={link}>
       <div className={clsx(styles.root, className)}>{children}</div>
