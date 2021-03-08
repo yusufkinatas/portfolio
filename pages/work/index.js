0478930733e1643b803b0b40dce34c56b7f35768
Yusuf Kınataş <yusufkinatas@gmail.com>
2021-03-08 18:53:48 +0300

    update works page (wip)

--- pages/work/index.js
+++ pages/work/index.js
@@ -8,15 +8,26 @@ function Work() {
   return (
     <Page>
       <div className={styles.root}>
+      <h2>Here is some of my best projects</h2>
         <div className={styles.projectList}>
           {projects.map(p => (
+            <a href={p.links[0].url} target="_" >
+            <div>
+              <div className={styles.project} style={{ background: p.color }}>
+                <img src={p.logo} />
+                <div className={styles.title}>{p.title}</div>
+              </div>
+            </div>
+            </a>
+          ))}
+          {/* {projects.map(p => (
             <Link key={p.id} href="/work/[id]" as={`/work/${p.id}`}>
               <div className={styles.project} style={{ background: p.color }}>
                 <img src={p.logo} />
-                <a>{p.title}</a>
+                <div className={styles.title}>{p.title}</div>
               </div>
             </Link>
-          ))}
+          ))} */}
         </div>
       </div>
     </Page>
